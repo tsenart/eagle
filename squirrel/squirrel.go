e1d6903d197e70d18a0ac33d88a425dbd52db166
Alexander Simmerl <alx@soundcloud.com>
2014-04-29 17:23:05 +0200

    Add custom squirrel metrics exposition

--- squirrel/squirrel.go
+++ squirrel/squirrel.go
@@ -6,14 +6,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/exp"
 )
 
 func main() {
 	var (
 		listen = flag.String("listen", ":7801", "Server listen address")
+
+		requestDuration  = prometheus.NewCounter()
+		requestDurations = prometheus.NewDefaultHistogram()
+		requestTotal     = prometheus.NewCounter()
 	)
 	flag.Parse()
 
@@ -22,11 +28,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	exp.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	prometheus.Register("squirrel_requests_total", "Total number of requests made", prometheus.NilLabels, requestTotal)
+	prometheus.Register("squirrel_requests_duration_nanoseconds_total", "Total amount of time squirrel has spent to answer requests in nanoseconds", prometheus.NilLabels, requestDuration)
+	prometheus.Register("squirrel_requests_duration_nanoseconds", "Amounts of time squirrel has spent answering requests in nanoseconds", prometheus.NilLabels, requestDurations)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		defer func(began time.Time, r *http.Request) {
+			d := time.Since(began)
+			labels := map[string]string{
+				"method": strings.ToLower(r.Method),
+				"path":   "/",
+				"code":   strconv.Itoa(http.StatusOK),
+			}
+
+			requestTotal.Increment(labels)
+			requestDuration.IncrementBy(labels, float64(d))
+			requestDurations.Add(labels, float64(d))
+		}(time.Now(), r)
+
 		fmt.Fprint(w, "OK")
 	})
-	exp.Handle(prometheus.ExpositionResource, prometheus.DefaultHandler)
+	http.Handle(prometheus.ExpositionResource, prometheus.DefaultRegistry.Handler())
 
 	log.Printf("Starting server on %s", *listen)
-	log.Fatal(http.ListenAndServe(*listen, exp.DefaultCoarseMux))
+	log.Fatal(http.ListenAndServe(*listen, nil))
 }
